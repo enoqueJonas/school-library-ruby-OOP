475c1361b1a811df715afa95f23e0e3cc167d2a5
enoqueJonas <enoque.macanda@gmail.com>
2023-02-13 19:50:33 +0200

    UPDATE person.rb adding methods is_of_age and can_use_services?

--- person.rb
+++ person.rb
@@ -1,10 +1,23 @@
 class Person
     attr_accessor :name, :age
     attr_reader :id
-    
+
     def initialize(name="Unknown", age, parent_permission=true)
         @id = Random.rand(0...1000)
         @name = name
         @age = age
     end
+
+    def is_of_age?
+        return true if @age >= 18
+        false
+    end
+
+    def can_use_services?
+        if(is_of_age? || parent_permission)
+            true
+        end
+        false
+    end
+    private :is_of_age?
 end
