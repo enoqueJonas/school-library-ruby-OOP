dcde5c9a377c5847e21461ab6a1b136ccff3e1b2
enoqueJonas <enoque.macanda@gmail.com>
2023-02-13 19:54:09 +0200

    UPDATE student.rb adding a class and a constructor method

---
+++ student.rb
@@ -0,0 +1,8 @@
+require './person.rb'
+
+class Student < Person
+    def initialize(name="Unknown", age, parent_permission=true, classroom)
+        super(name, age, parent_permission)
+        @classroom = classroom
+    end
+end
