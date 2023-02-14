de3ef0691c1146f1e82cf4364922b11aa86b0c01
enoqueJonas <enoque.macanda@gmail.com>
2023-02-14 21:56:29 +0200

    IMPROVE nameable_decorator.rb, capitalized_decorator.rb

--- main.rb
+++ main.rb
@@ -3,8 +3,8 @@ require './decorators/captilized_decorator'
 require './decorators/trim_decorator'
 
 person = Person.new(22, 'maximilianus')
-person.correct_name
+p person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
+p capitalized_person.correct_name
 capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
 capitalized_trimmed_person.correct_name
