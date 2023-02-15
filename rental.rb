c4b82f8557a6d0b4696f3dc5fdcd2b8911e3b4f8
enoqueJonas <enoque.macanda@gmail.com>
2023-02-15 18:32:17 +0200

    UPDATE rental.rb to have Book and Person in it's contructor

--- rental.rb
+++ rental.rb
@@ -1,8 +1,10 @@
 class Rental
     attr_accessor :date, :book, :person
 
-    def initialize(date)
+    def initialize(date, book, person)
         @date = date
+        @book = book
+        @person = person
     end
 
     def book=(book)
