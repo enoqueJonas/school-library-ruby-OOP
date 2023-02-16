007d9f9a3e024b6d2313f02655c46095f6b72b02
enoqueJonas <enoque.macanda@gmail.com>
2023-02-16 22:55:01 +0200

    UPDATE method app.rb

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require_relative './book.rb'
 require_relative './classroom.rb'
 require_relative './student.rb'
+require_relative './rental.rb'
 
 class App
   def initialize
     @books = []
     @people = []
     @classroom = nil
+    @rentals = []
   end
 
   def create_book
@@ -16,6 +18,7 @@ class App
     author = gets.chomp
     book = Book.new(title, author)
     @books << book
+    puts "Book created succesfully"
   end
 
   def create_person
@@ -36,6 +39,7 @@ class App
         stu = Student.new(age, @classroom, name, permission)
 
         @people << stu
+        puts "Student created succesfully"
     elseif answer == 2
         puts "Name: "
         name = gets.chomp
@@ -44,6 +48,44 @@ class App
         puts "Specialization:  "
         teacher = Teacher.new(age, specialization, name)
         @people << teacher
+        puts "Teacher created succesfully"
+    else
+        puts "Invalid choice"
+        create_person()
     end
+  end
+
+  def list_books
+    @books.each_with_index do |book, index|
+      puts "#{index}) #{book}"
+    end
+  end
+
+  def list_people
+    @people.each_with_index do |person, index|
+      puts "#{index}) #{person}"
+    end
+  end
+
+  def list_rentals(id)
+    @rentals.each_with_index do |rental|
+      puts "#{index}) #{rental}" if id == rental.person.id
+    end
+  end
+
+  def create_rental
+    puts "Select a book from the following by number (not id): "
+    list_books()
+    book_nr = gets.chomp
+    book = @books[book_nr]
+    puts "Select a person from the following by number (not id): "
+    list_people()
+    person_nr = gets.chomp
+    person = @people[person_nr]
+    puts "Date: "
+    date = gets.chomp
+    rental = Rental.new(date, book, person)
+    @rentals << rental
+    puts "Rental created succesfully"
   end
 end
