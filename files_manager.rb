d179aba1e1d6e799260e1aa714fb1ecee310a3e5
enoqueJonas <enoque.macanda@gmail.com>
2023-02-20 23:00:38 +0200

    UPDATE files_manager.rb to be a module

--- files_manager.rb
+++ files_manager.rb
@@ -1,12 +1,14 @@
 require 'json'
 
-def write_on_file(file_name, data) 
-    json = JSON.generate(data)
-    File.write('file_name', json, mode: "a")
-end
-
-
-def read_file(file_name)
-    data = JSON.parse(File.read(file_name))
-    data
+module FileManage
+    def write_on_file(file_name, data) 
+        json = JSON.generate(data)
+        File.write('file_name', json, mode: "a")
+    end
+    
+    
+    def read_file(file_name)
+        data = JSON.parse(File.read(file_name))
+        data
+    end
 end
